01f892039cced89dc70186f41e9b144310759c5d
Pon Cheol Ku <bbon@bbon.kr>
2019-06-28 14:13:42 +0900

    UPDATED: - /me/settings 페이지에 정보변경, 비밀번호 변경, 호원탈퇴 링크 추가

--- src/pages/me/settings.js
+++ src/pages/me/settings.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Link from 'next/link';
-import { PageHeader, Button } from 'antd';
+import { PageHeader, Button, Divider, Typography } from 'antd';
 import { withAuth } from '../../utils/auth';
 import MeLayout from '../../components/MeLayout';
 import { ContentWrapper } from '../../styledComponents/Wrapper';
@@ -10,13 +10,42 @@ const Settings = () => {
         <MeLayout>
             <ContentWrapper>
                 <PageHeader title="Settings" />
-
+                <Divider />
                 <div>
-                    <Link href="/me/unregister">
-                        <a>
-                            <Button type="danger">Unregister</Button>
-                        </a>
-                    </Link>
+                    <div>
+                        <Typography.Title level={3}>
+                            Change account information
+                        </Typography.Title>
+                        <Link href="/me/changeinfo">
+                            <a>
+                                <Button type="default">
+                                    Change account information
+                                </Button>
+                            </a>
+                        </Link>
+                    </div>
+                    <Divider dashed={true} />
+                    <div>
+                        <Typography.Title level={3}>
+                            Change password
+                        </Typography.Title>
+                        <Link href="/me/changepassword">
+                            <a>
+                                <Button type="default">Change password</Button>
+                            </a>
+                        </Link>
+                    </div>
+                    <Divider dashed={true} />
+                    <div>
+                        <Typography.Title level={3}>
+                            Unregister
+                        </Typography.Title>
+                        <Link href="/me/unregister">
+                            <a>
+                                <Button type="danger">Unregister</Button>
+                            </a>
+                        </Link>
+                    </div>
                 </div>
             </ContentWrapper>
         </MeLayout>
